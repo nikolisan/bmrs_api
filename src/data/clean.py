45859ab90a4797f8a41788c98258bf8f44a65dd8
Nick Andreakos <18707825+nikolisan@users.noreply.github.com>
2026-04-20 15:43:10 +0100

    Fixed issue with more or less than 48 periods

--- src/data/clean.py
+++ src/data/clean.py
@@ -1,4 +1,6 @@
+from datetime import date, time, datetime, timedelta
 from typing import Any
+from zoneinfo import ZoneInfo
 
 import pandas as pd
 from loguru import logger
@@ -6,6 +8,17 @@ from loguru import logger
 from src.api.models import SystemPriceRecord
 
 logger.add("data_clean.log", rotation="10 MB", level="WARNING")
+
+
+def _expected_periods(settlement_date: datetime) -> int:
+    """
+    Helper function to calculate the expected number of 30min periods in a day.
+    Returns 48 for normal days, 46 for March and 48 October BST shift.
+    """
+
+    start = datetime.combine(settlement_date, time.min, tzinfo=ZoneInfo.no_cache("Europe/London"))
+    end = datetime.combine(settlement_date + timedelta(days=1), time.min, tzinfo=ZoneInfo.no_cache("Europe/London"))
+    return int((end - start).total_seconds() // 1800)
 
 
 def create_system_price_dataframe(settlement_periods: list[SystemPriceRecord]) -> pd.DataFrame:
@@ -21,9 +34,12 @@ def create_system_price_dataframe(settlement_periods: list[SystemPriceRecord]) -
     df = df.set_index("settlementPeriod")
     df = df.sort_index()
 
-    # TODO: Why some days have 48 and other 50 SPs?
-    if len(df) != 48:
-        raise ValueError(f"{df.iloc[0]["settlementDate"]}: Expected 48 SP, got {len(df)}.")
+    # FIXED: Why some days have 48 and other 50 SPs? It has to do with the BST. Last Sunday of March has 46, last Sunday Oct has 50
+    _date = df.iloc[0]["settlementDate"]
+    _expected = _expected_periods(_date)
+    
+    if len(df) > _expected:
+        raise ValueError(f"{_date}: Expected {_expected} SP, got {len(df)}.")
     
     # Fill missing price values
     df["is_interpolated"] = df[["systemSellPrice", "systemBuyPrice", "netImbalanceVolume"]].isna().any(axis=1)
